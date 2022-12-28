c99340a9782582fedb53c358f45e03e50d48d2ec
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-29 00:04:00 +0200

    finish task

--- src/index.js
+++ src/index.js
@@ -13,45 +13,47 @@ input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(evt) {
     const name = evt.target.value;
-    fetchCountries(name)
-    .then(obj => {
-    if (obj.length > 10) {
-        Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
-    } else {createMarkup(obj)}
-    })
-    .catch(error => Notiflix.Notify.failure('Oops, there is no country with that name'));
-}
-
-function addMarkup(markupList, markup) {
-    if (markupList) {
-        countryList.innerHTML = markupList.join('');
-    } else {
-        countryInfo.innerHTML = markup.join('');
+    if (name) {
+        fetchCountries(name.trim())
+        .then(obj => {
+        if (obj.length >= 10) {
+            Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+            countryInfo.innerHTML = '';
+            countryList.innerHTML = '';
+        } else {createMarkup(obj)}
+        })
+        .catch(error => Notiflix.Notify.failure('Oops, there is no country with that name'));
     }
 }
 
 function createMarkup(obj) {
-
-    if(obj.length > 1 ){
-        const markupList = obj.map(obj => 
-            `<li>
-              <p><img src="${obj.flags.svg}" alt="${obj.name.common}" width = "15">${obj.name.official}</p>
+    if(obj.length >= 2){
+        const markup = obj.map(obj => 
+            `<li class="countries">
+              <p>
+              <img src="${obj.flags.svg}" alt="${obj.name.common}" width = "30"> &nbsp
+              ${obj.name.official}
+              </p>
             </li>`
             );
-        addMarkup(markupList)
+            countryList.innerHTML = markup.join('');
+            countryInfo.innerHTML = '';
         
     } else {
         const markup = obj.map(({ flags, name, capital, population, languages }) => 
-            `<h2>
-              <img src="${flags.svg}" alt="${name.common}" width = "35">
-              ${name.official}
-            </h2>
-            <div>
-              <p><b>Capital:&nbsp</b><span>${capital}</span></p>
-              <p><b>Population:&nbsp</b><span>${population}</span></p>
-              <p><b>Languages:&nbsp</b><span>${Object.values(languages).join(", ")}</span></p>
+            `<div class="card">
+              <h2>
+                <img src="${flags.svg}" alt="${name.common}" width = "45"> &nbsp
+                ${name.official}
+              </h2>
+              <div>
+                <p><b>Capital:&nbsp</b><span>${capital}</span></p>
+                <p><b>Population:&nbsp</b><span>${population}</span></p>
+                <p><b>Languages:&nbsp</b><span>${Object.values(languages).join(", ")}</span></p>
+              </div>
             </div>`
             );
-        addMarkup(markup)
-    }
+            countryInfo.innerHTML = markup.join('');
+            countryList.innerHTML = '';
+        } 
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,14 +3,11 @@ function fetchCountries(country) {
     const BASE_URL = 'https://restcountries.com/v3.1/name';
     return fetch(`${BASE_URL}/${country}?fields=${options}`)
     .then(resp => {
-        console.log(resp);
         if(!resp.ok){
             throw new Error(resp.statusText);
         }
         return resp.json();
     });
-    
-    // return response.then(resp => resp.json()).then(data => console.log(data));
 }
 
 export {fetchCountries};
